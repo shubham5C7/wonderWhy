3c556293498c10c03a5e43bca5a2851bc3956c65
shubham5C7 <shubhamraopatil123@gmail.com>
2026-04-22 20:12:35 +0530

    fix: cors and cookie for production

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const gameRoutes = require("./routers/game.router");
 const port = process.env.PORT || 8000;
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175","https://wonder-why-seven.vercel.app",], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -31,7 +31,8 @@ const io = new Server(server, {
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      ,"http://localhost:5175"
+      ,"http://localhost:5175",
+      "https://wonder-why-seven.vercel.app",
     ],
     credentials: true,
   },

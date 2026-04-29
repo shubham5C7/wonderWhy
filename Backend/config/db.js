b56b57ee4fd63d41edadb1dce90b274f7ae1c177
shubham5C7 <shubhamraopatil123@gmail.com>
2026-04-29 16:36:44 +0530

    switch to postgresql

---
+++ Backend/config/db.js
@@ -0,0 +1,13 @@
+const pkg = require("pg");
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 5432,
+  ssl: { rejectUnauthorized: false },
+});
+
+module.exports = { pool };
--- Backend/server.js
+++ Backend/server.js
@@ -1,30 +1,34 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const http = require("http");                          
-const { Server } = require("socket.io");               
-const { setupSocket } = require("./socket/index");
-const userRoutes = require("./routers/user.router");
-const gameRoutes = require("./routers/game.router");  
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import http from "http";
+import { Server } from "socket.io";
+
+import { setupSocket } from "./socket/index.js";
+import userRoutes from "./routers/user.router.js";
+import gameRoutes from "./routers/game.router.js";
 
 const port = process.env.PORT || 8000;
 const app = express();
 
+// Middleware
 app.use(cors({ origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175",], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes
 app.use("/api/auth", userRoutes);
 app.use("/api/game", gameRoutes); 
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log("MongoDB error", err.message));
-
+// HTTP and  Socket server
 const server = http.createServer(app);
 
+pool.query("SELECT NOW()")
+.then(()=>console.log("PostgerSQl Connected AWS RDC"))
+.catch((err)=>console.error("DB Connection Failed",err.message))
 
 const io = new Server(server, {
   cors: {
@@ -32,11 +36,10 @@ const io = new Server(server, {
       "http://localhost:5173",
       "http://localhost:5174",
       ,"http://localhost:5175",
-    
     ],
     credentials: true,
   },
 });
 setupSocket(io);
-
+// Start server
 server.listen(port, () => console.log(`Server running on ${port}`));
